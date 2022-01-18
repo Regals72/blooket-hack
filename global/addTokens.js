25b7b598f8f185316f27f82b6657df1e29d194b6
Regals72 <97985046+Regals72@users.noreply.github.com>
2022-01-18 16:17:15 -0500

    Update addTokens.js

--- global/addTokens.js
+++ global/addTokens.js
@@ -13,10 +13,10 @@ async function getName() {
 };
 
 async function addCurrencies() {
-    const tokens = Number(prompt('How many tokens do you want to add to your account? (500 daily)'));
+    const tokens = Number(prompt('How many tokens do you want to add to your account? (1500 hourly)'));
 
-    if (tokens > 500) {
-        alert('You can only add up to 500 tokens daily.');
+    if (tokens > 1500) {
+        alert('You can only add up to 1500 tokens daily.');
     };
 
     const response = await fetch('https://api.blooket.com/api/users/add-rewards', {
